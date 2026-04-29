d15bc2e07a93a45656decc9e53c3967e59d5fda9
darioDBJ <ddiazbaez@frba.utn.edu.ar>
2026-04-29 18:17:48 -0300

    Soluciona conflictos de integración

--- operaciones.c
+++ operaciones.c
@@ -4,7 +4,7 @@
 
 int sumar(int a, int b) {
     return a + b;
-}//hola mundo
+}
 
 /* ── restar — ya implementada ────────────────────────────────────────────── */
 
@@ -13,10 +13,9 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-<<<<<<< HEAD
-=======
 /*La función recibe dos variables y las multiplica entre si para devolver ese resultado como valor. Debe respetarse un orden específico en la multiplicación de las variables*/
->>>>>>> 83cb18e3c4d17bacf8dde717b3c4a3d4ccb5d5e3
+
+
 int multiplicar(int a, int b) {
     return a * b;
 }
@@ -25,8 +24,4 @@ int multiplicar(int a, int b) {
 
 int esPar(int n) {
     return (n % 2) == 0;
-}
-
-int dividir(int a, int b) {
-    return a - b; /* bug intencional */
 }
